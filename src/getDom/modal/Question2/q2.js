384f9d2f9f32565cf430bbff7354da5d3870be2b
Ura20 <ura2091.212@gmail.com>
2025-01-18 16:55:02 +0900

    feat:Dom-Basics

--- src/getDom/modal/Question2/q2.js
+++
@@ -1,4 +0,0 @@
-// DOMの取得をしてみよう！
-// __の箇所に適切なものを書きましょう！
-const signinButton = document.querySelector('.signin-button');
-console.log(__);
--- src/getDom/modal/Question2/q5.js
+++
@@ -1,4 +0,0 @@
-// DOMの取得をしてみよう！
-// __の箇所に適切なものを書きましょう！
-const signinButton = __;
-console.log(__);
